21a003cc9fbfca4f0098f03cefa1272fed96326c
lilF <delo5070@gmail.com>
2022-07-29 09:57:38 +0400

    merge_sort, qsort

--- 05_merge_sort.py
+++ 05_merge_sort.py
@@ -4,7 +4,7 @@ def merge(A: list, B: list):
     C = [0] * (len(A) + len(B))
     i = k = n = 0
     while i < len(A) and k < len(B):
-        if A[i] < B[k]:
+        if A[i] <= B[k]:
             C[n] = A[i]
             i += 1
             n += 1
@@ -24,16 +24,33 @@ def merge(A: list, B: list):
         n += 1
 
     return C
-            
-            
-# Рекурсивная функция
 
+
+# Рекурсивная функция
 def merge_sort(A):
     if len(A) <= 1:
         return
-    
-            
+    middle = len(A)//2
+    L = [A[i] for i in range(0, middle)]
+    R = [A[i] for i in range(middle, len(A))]
+    merge_sort(L)
+    merge_sort(R)
+    C = merge(L, R)
+    for i in range(len(A)):
+        A[i] = C[i]
+
+
+def get_randomlist(n, m):
+    randomlist = []
+    for i in range(0, n):
+        n = random.randint(1, m)
+        randomlist.append(n)
+    return randomlist
+
+
 if __name__ == '__main__':
-    A = [random.randint(0, 5)]
-    B = [random.randint(0, 5)]
-    print(merge(A, B))
+    A = get_randomlist(5, 20)
+    B = get_randomlist(5, 20)
+    merge_sort(B)
+    print(*B)
+    
---
+++ 05_qsort_Hoare.py
@@ -0,0 +1,45 @@
+# Сортировка Тони Хоара (быстрая сортировка)
+
+def hoare_sort(A: list):
+    if len(A) <= 1:
+        return None
+    barrier = A[0]
+    L = []
+    M = []
+    R = []
+    for x in A:
+        if x < barrier:
+            L.append(x)
+        elif x == barrier:
+            M.append(x)
+        else:
+            R.append(x)
+    hoare_sort(L)
+    hoare_sort(R)
+    k = 0
+    for x in L + M + R:
+        A[k] = x
+        k += 1
+        
+        
+def check_sorted(A, assending=True):
+    '''
+    Проверка отсортированости за О(len(A)=O(n))
+    '''
+    flg = True
+    N = len(A)
+    s = 2 * int(assending)-1
+    # for i in range(0, N-1):
+        # if A[i] > A[i+1]:
+        #     flg = False
+        # break
+    # return flg
+    # int(True) = 1
+    # int(False) = 0
+    # s = 2 * x - 1
+    for i in range(0, N-1):
+        if s * A[i] > s * A[i+1]:
+            flg = False
+            break
+        return flg
+    
