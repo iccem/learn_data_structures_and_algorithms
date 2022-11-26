5dfeb015c551a52bb915a0571c87fed66acc2919
lilF <delo5070@gmail.com>
2022-11-26 10:32:22 +0400

    fix

--- 03_count-sort.py
+++ 03_count-sort.py
@@ -3,15 +3,19 @@
 # массив частот frequency
 
 def count_sort(A):
-    ''' Сортировка массива чисел методом подсчета '''
+    ''' Сортировка массива чисел методом подсчета. O(N).
+    O(M) -  по памяти, где M - число различных элементов.
+    Быстро сортирует большие объемы информации.
+    Нам должен быть известет диапазон частот и он должен быть небольшим.'''
     N = len(A)
-    F = [0] * 10
+    F = [0] * 10 # массив частот
     for i in range(N):
         x = int(input())
         F[x] += 1
+    print(F)
 
-def test_count_sort(sort_algoriths):
-    print('testcase#1: ', end='')
+def test_count_sort(sort_algorithm):
+    print('testcase #1: ', end='')
     print('Testing: ', count_sort.__doc__)
     A = [6, 8, 1, 9, 2, 7, 3, 5, 4]
     A_sorted = list(range(1, 10))
@@ -19,4 +23,8 @@ def test_count_sort(sort_algoriths):
     print('OK' if A == A_sorted else 'Fail')
     
 if __name__ == '__main__':
-    test_count_sort(count_sort)
+    # test_count_sort(count_sort)
+    
+    A = [6, 8, 1, 9, 2, 7, 3, 5, 4]
+    count_sort(A)
+    # print(A)
