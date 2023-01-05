e7f0f1c644dd2fc3e8d1d21d52f06c428346b3ab
lilF <delo5070@gmail.com>
2023-01-05 08:33:52 +0400

    fix

--- temp.py
+++ temp.py
@@ -1,10 +1,32 @@
-class Someone:
-    a = 0
-    
-    def func(self):
-        a = 1
-        
-b = Someone()
+'''
+git clone
+cd repo
+vim
+git status
+git diff
 
-print('-'* 20)
-print(f'print: {b.a}')
+git stash   # запомнить и спрятать изменения, главное их там не забыть
+            # git stash pop - достать из "кармашка"
+git pull
+git stash pop
+# пристыковка получается бесшовная без коммита
+
+git add
+git commit
+
+git pull
+
+git push
+'''
+
+
+
+'''
+Событийно ориентированное программирование
+
+pygame
+tkinter
+qt
+
+
+'''
